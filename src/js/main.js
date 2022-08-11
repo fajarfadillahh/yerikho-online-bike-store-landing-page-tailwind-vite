7c0119bf91346d31654d17089b6081c6f633f7df
fajarfadillahh <fajaragustian979@gmail.com>
2022-08-11 09:15:37 +0700

    faq section

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,31 @@ function showMenu(menuId, toggleId) {
   }
 }
 showMenu("header-menu", "header-toggle");
+
+// ===== ACCORDION FAQ SECTION =====
+const accordionItems = document.querySelectorAll(".faq__accordion-item");
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".faq__accordion-header");
+
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".faq__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
